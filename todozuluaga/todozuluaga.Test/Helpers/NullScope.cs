b2fb4090a48b5f81e6e0672437e6a5857538bc4c
jaenmurray <jaencarlosmontanomurray@gmail.com>
2021-08-23 00:32:32 -0500

    Unit test preparation

---
+++ todozuluaga/todozuluaga.Test/Helpers/NullScope.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace todozuluaga.Test.Helpers
+{
+    public class NullScope : IDisposable
+    {
+        public static NullScope Instance { get; } = new NullScope();
+        public void Dispose() { }
+        private NullScope() { }
+    }
+}
